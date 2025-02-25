3936c855cc0e0ec770b6befe7d31cec82a97df5b
gnak <gnakzz@qq.com>
2025-02-25 15:56:17 +0800

    style: 样式调整

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,10 +5,10 @@
   <div class="flex flex-col">
     <ul>
       <li>
-        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/">首页</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/about">关于</NuxtLink>
       </li>
     </ul>
     <slot />
